f4f5ecdb4da446e9c6e92bb27494080a8def9d0a
Greg <hello@gregmiller.io>
2020-12-21 14:50:03 -0500

    Add from 2017 default theme * Referenced from elements.css, was missing in filesystem

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+	 * Search form adapted from 2017 theme
+	*/
+	$unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
+
+<form method="get" class="form-inline my-2 my-lg-0" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="search" id="<?php echo $unique_id; ?>" class="form-control mr-sm-2"
+		   placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'sapphire-wp' ); ?>" <?php echo get_search_query(); ?>
+		   name="s">
+	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
