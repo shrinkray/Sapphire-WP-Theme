72e096b4d7fe433b064c1b90c22a2581af2b64fb
Greg <hello@gregmiller.io>
2020-12-24 18:00:16 -0500

    Refactor webpack.mix.js * Add cssnano option packages * add postcss-url package

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,38 @@
+/**
+ * @date 12-24-2020
+ * @author Greg
+ * TODO: Having problem with URL Processing
+ * Help may be here: https://github.com/postcss/postcss-url
+ * I think there may be better ways to handle the image URLs via Tailwinds and SCSS approach
+ * Because this test is an existing theme with preexisting file paths for code we are not even using
+ *   I dealt with the background images in elements.css .. Hopefully in a site built with SCSS,
+ *   URL processing will be effective
+ */
+
 let mix = require('laravel-mix');
-//require('laravel-mix-postcss-config');
+require('laravel-mix-postcss-config');
+
+// Commented out assignments are from the postcss-url plugin page example
+// const fs = require("fs")
+// const postcss = require("postcss")
+// const url = require("postcss-url")
+//
+// // css to be processed
+// const css = fs.readFileSync("layout/css/app.css", "utf8")
 
 mix
-    //  .js('layout/js/app.js', 'dist/')
     .js('layout/js/app.js', 'dist/app.js')
-    .postCss('layout/css/layout.css', 'dist/app.css', [
+    .options({
+        processCssUrls: true
+    })
+    .css('style.css', 'dist/style.css')
+    .postCss('layout/css/app.css', 'dist/app.css', [
         require('postcss-custom-properties'),
         require('postcss-sorting')({
             'properties-order': 'alphabetical'
+        }),
+        require('postcss-url')({
+         // Seeking options that work with Mix
         }),
         require('cssnano')
     ])
